f0081e4dd3085358d31e2a8a2e950f4dc36239e2
Jaime Rojas <rojas@safenow.de>
2025-08-11 14:17:04 +0200

    handle notification

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,2 @@
 // service-worker.js for Pushy Web Push
-importScripts('https://sdk.pushy.me/web/1.0.13/pushy-service-worker.js');
+importScripts("https://sdk.pushy.me/web/1.0.13/pushy-service-worker.js");
